aedcfc95893a67313c6e8c98b6720fc32ed8ed26
jahidultr <jahidulislamdiu64@gmail.com>
2024-08-30 02:39:06 +0600

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,252 @@
 import 'package:flutter/material.dart';
-import 'package:responsivedesign/presentation/screens/home_screen.dart';
 
-void main(){
-  runApp(RSAApp());
+void main() {
+  runApp(MyApp());
 }
-class RSAApp extends StatelessWidget {
-  const RSAApp({super.key});
 
+class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
+      title: 'HUMMING\n BIRD',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: ResponsiveHomePage(),
+    );
+  }
+}
+
+class ResponsiveHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: LayoutBuilder(
+        builder: (context, constraints) {
+          if (constraints.maxWidth > 1600) {
+            return DesktopView();
+          } else if (constraints.maxWidth > 800 &&
+              constraints.maxWidth < 1600) {
+            return TabletView();
+          } else {
+            return MobileView();
+          }
+        },
+      ),
+    );
+  }
+}
+
+class DesktopView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        NavigationDrawer(),
+        Expanded(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  'FLUTTER WEB. THE BASICS',
+                  style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(height: 20),
+                const Text(
+                  'In this course we will go over the basics of using Flutter '
+                  'Web for development. Topics will include Responsive Layout, Deploying,'
+                  ' Font Changes, Hover functionality, Modals and more.',
+                  style: TextStyle(fontSize: 24),
+                ),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  style:
+                      ElevatedButton.styleFrom(backgroundColor: Colors.green),
+                  onPressed: () {},
+                  child: const Text(
+                    'Join course',
+                    style: TextStyle(color: Colors.black),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class TabletView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        NavigationBar(),
+        Expanded(
+          child: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                const Text(
+                  'FLUTTER WEB. THE BASICS',
+                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(height: 20),
+                const Text(
+                  'In this course we will go over the basics of using Flutter Web for development. Topics will include Responsive Layout, Deploying, Font Changes, Hover functionality, Modals and more.',
+                  style: TextStyle(fontSize: 16),
+                ),
+                const SizedBox(height: 20),
+                ElevatedButton(
+                  style:
+                      ElevatedButton.styleFrom(backgroundColor: Colors.green),
+                  onPressed: () {},
+                  child: const Text(
+                    'Join course',
+                    style: TextStyle(color: Colors.black),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class MobileView extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('HUMMING\n BIRD'),
+      ),
+      drawer: Drawer(
+        // backgroundColor: Colors.grey,
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.green,
+              ),
+              child: Column(
+                children: [
+                  Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        'SKILL UP NOW',
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontStyle: FontStyle.normal,
+                        ),
+                      ),
+                      SizedBox(width: 5),
+                      Text(
+                        'TAP HERE',
+                      )
+                    ],
+                  ),
+                ],
+              ),
+            ),
+            ListTile(
+              title: const Text('Episodes'),
+              onTap: () {},
+            ),
+            ListTile(
+              title: const Text('About'),
+              onTap: () {},
+            ),
+          ],
+        ),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    'FLUTTER WEB. THE BASICS',
+                    style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(height: 20),
+                  const Text(
+                    'In this course we will go over the basics of using Flutter Web for development. Topics will include Responsive Layout, Deploying, Font Changes, Hover functionality, Modals and more.',
+                    style: TextStyle(fontSize: 16),
+                  ),
+                  Column(
+                    children: [
+                      const SizedBox(height: 20),
+                      ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                            backgroundColor: Colors.green),
+                        onPressed: () {},
+                        child: const Text(
+                          'Join course',
+                          style: TextStyle(color: Colors.black),
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class NavigationDrawer extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          const DrawerHeader(
+            child: Text('SKILL UP NOW'),
+            decoration: BoxDecoration(
+              color: Colors.blue,
+            ),
+          ),
+          ListTile(
+            title: const Text('Episodes'),
+            onTap: () {},
+          ),
+          ListTile(
+            title: const Text('About'),
+            onTap: () {},
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class NavigationBar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      title: const Text('HUMMING\n BIRD'),
+      actions: [
+        TextButton(
+          onPressed: () {},
+          child: const Text('Episodes', style: TextStyle(color: Colors.black)),
+        ),
+        TextButton(
+          onPressed: () {},
+          child: const Text('About', style: TextStyle(color: Colors.black)),
+        ),
+      ],
     );
   }
 }
